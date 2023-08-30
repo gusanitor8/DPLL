a594e7f188f089a4cc44f5d34fa0e3d9fbbcc802
gusanitor8 <gusanitor8@gmail.com>
2023-08-30 00:36:22 -0600

    version terminada

--- proyecto2.py
+++ proyecto2.py
@@ -1,7 +1,7 @@
 from sympy import symbols, Not
 
 
-def DPLL(B, I):
+def DPLL(B, I:dict):
 
     #Si B es vacia, entonces regresar True e I
     if not B:
@@ -29,14 +29,15 @@ def DPLL(B, I):
         if L not in clause and Not(L) not in clause:
             B_prime.append(clause)            
         elif Not(L) in clause:
-            new_clause = set()
+            new_clause = {}
             for literal in clause:        
                 if literal != Not(L):
-                    new_clause.add(literal)
+                    new_clause[literal] = None
             B_prime.append(new_clause)
 
     #I’ = I ∪ {valor de L es verdadero}
-    I_prime = I.union({L: True})
+    I_prime = I.copy()
+    I_prime[L] = True
 
     #resultado e I1 ← DPLL(B’,I’)
     result, I1 = DPLL(B_prime, I_prime)
@@ -51,13 +52,14 @@ def DPLL(B, I):
         if L not in clause and Not(L) not in clause:
             B_prime.append(clause)            
         elif L in clause:
-            new_clause = set()
+            new_clause = {}
             for literal in clause:        
                 if literal != L:
-                    new_clause.add(literal)
+                    new_clause[literal] = None
             B_prime.append(new_clause)
 
-    I_prime = I.union({L: False})
+    I_prime = I.copy()
+    I_prime[L] = False
 
     result, I2 = DPLL(B_prime, I_prime)
 
@@ -67,9 +69,52 @@ def DPLL(B, I):
     return False, None
     
 
-
+#ejemplo 1
 P,Q,R,S = symbols('P Q R S')
-formula = [{Not(P) , Not(R) , Not(S)}, {Not(Q) , Not(P), Not(S)}]
-result, assignment = DPLL(formula, set())
-print(result)
-print(assignment)
+formula1 = [
+    {Not(P): None , Not(R): None , Not(S): None}, 
+    {Not(Q): None , Not(P): None, Not(S): None}
+]
+
+#ejemplo 2
+formula2 = [
+    {Not(P): None, Not(Q): None},
+    {Q: None, Not(S): None},
+    {Not(P): None, S: None},
+    {Not(Q): None, S: None}
+]
+
+#ejemplo 3
+formula3 = [
+    {Not(P): None, Not(Q): None, Not(R): None},
+    {Q: None, Not(R): None, P: None},
+    {Not(P): None, Q: None, R: None}
+]
+
+#ejemplo 4
+formula4 = [
+    {R: None},
+    {Not(Q): None, Not(R): None},
+    {Not(P): None, Q: None, Not(R): None},
+    {Q: None}
+]
+
+#ejemplo 5
+formula5 = [
+    {Q: None, P: None, Not(P): None}
+]
+
+#ejemplo 6
+formula6 = [
+    {P: None},
+    {Not(P): None}
+]
+
+formulaArr = [formula1, formula2, formula3, formula4, formula5, formula6]
+
+for formula in formulaArr:
+    print(formula)
+    result, assignment = DPLL(formula, {})
+    print(result)
+    print(assignment)
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
